edda19c13429057e38577cb4ab37a5f0db5f8244
bazjerc <80469836+Bazjerc@users.noreply.github.com>
2021-03-13 15:34:19 +0100

    create object containing message parts

---
+++ mixed_messages.js
@@ -0,0 +1,13 @@
+const userName = '' || 'Stranger';
+
+// contains options for each part of the message
+const trainingPlan = {
+  dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  timeOfDay: ['6:00', '8:00', '10:00', '12:00', '14:00', '16:00', '18:00'],
+  bodyPart: ['upper body', 'lower body', 'whole body'],
+  weight: ['light', 'heavy'],
+  reason: ['get the blood pumping', 'maintain your strength', 'improve your strength', 'gain muscle mass'],
+  motivationMessage: ['go hard or go home', 'your body will thank you']
+};
+
+
